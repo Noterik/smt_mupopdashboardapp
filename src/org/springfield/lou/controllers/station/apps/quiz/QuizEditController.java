143e67d3ff3cb26edf4cf6b00a2d046ee4c9e92d
Daniel Ockeloen <daniel@noterik.nl>
2018-08-27 10:32:33 +0200

    fixed a editor bug flaps didn't work because i copied from trivia game

--- src/org/springfield/lou/controllers/station/apps/quiz/QuizEditController.java
+++ src/org/springfield/lou/controllers/station/apps/quiz/QuizEditController.java
@@ -25,30 +25,26 @@ import org.json.simple.JSONObject;
 import org.springfield.fs.FsNode;
 import org.springfield.fs.FsPropertySet;
 import org.springfield.lou.controllers.Html5Controller;
-import org.springfield.lou.controllers.roominfo.RoomInfoController;
 import org.springfield.lou.controllers.station.apps.generic.ContentSelectEditController;
+import org.springfield.lou.controllers.station.apps.generic.SettingScreenEditController;
 import org.springfield.lou.controllers.station.apps.generic.WaitScreenEditController;
+import org.springfield.lou.controllers.station.apps.photoexplore.PhotoExploreMainAppController;
 import org.springfield.lou.model.ModelEvent;
 import org.springfield.lou.screen.Screen;
 
 public class QuizEditController extends Html5Controller{
-	
-	private String tab="mainapp";
+	private String tab="waitscreen";
 	private String valuelist="appeditor_content_url";
 	
-	public QuizEditController() {
-		
+	public QuizEditController() {	
 	}
 	
 	public void attach(String sel) {
 		selector = sel;
-		getVars();
 		fillPage();
 	}
 	
-	private void getVars() {
-		model.setProperty("/screen['data']/form['1']/appeditor_content_url",model.getProperty("@station/url"));
-	}
+
 	
 	private void fillPage() {
 		JSONObject data = new JSONObject();
@@ -60,8 +56,19 @@ public class QuizEditController extends Html5Controller{
 		screen.get(selector).render(data);
  		screen.get(".appmenu").on("mouseup",valuelist,"onAppMenu", this);
  		//screen.get(selector).on("mouseleave",valuelist,"onLeaveMenu", this);
-		screen.get("#subeditor").append("div","appeditor_subeditor_mainapp_quiz",new QuizMainAppController());
-
+ 		fillSubPage(tab);
+	}
+	
+	private void fillSubPage(String tab) {
+		if (tab.equals("setting")) {
+			screen.get("#subeditor").append("div","appeditor_subeditor_setting",new SettingScreenEditController());
+		} else if (tab.equals("waitscreen")) {
+			screen.get("#subeditor").append("div","appeditor_subeditor_waitscreen",new WaitScreenEditController());
+		} else if (tab.equals("contentselect")) {
+			screen.get("#subeditor").append("div","appeditor_subeditor_contentselect",new ContentSelectEditController());
+		} else if (tab.equals("mainapp")) {
+			screen.get("#subeditor").append("div","appeditor_subeditor_mainapp_quiz",new QuizMainAppController());
+		}
 	}
 	
 	
@@ -71,3 +78,7 @@ public class QuizEditController extends Html5Controller{
 	}
 	
 }
+
+
+
+
